eaf015d604cd13709d18b95fefe9d095c099accb
Robert J Serrano <wolfieca.rs@gmail.com>
2019-05-27 20:53:27 -0700

    More of the initial trickle

--- Wolf1.Core/Management/IDebtor.cs
+++ Wolf1.Core/Management/IDebtor.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Wolf1.Core.Management
 {
+    /// <summary>
+    /// The IDebtor interface represents individual debtors and the operations 
+    /// that they can perform
+    /// </summary>
     interface IDebtor
     {
+        int DebtorNumber { get; set; }
+        String LastName { get; set; }
+        String FirstName { get; set; }
+        Decimal CurrentBalance { get; set;}
+        Decimal OriginalBalance { get; set; }
+        IDemographics Demographics { get; set; }
+        IHistory History { get; set; }
+        IClient PrimaryClient { get; set; }
+        IPayment Payments { get; set; }
+        Legal.ILegalAction LegalActions { get; set; }
+        ICollector Collector { get; set; }
     }
 }
