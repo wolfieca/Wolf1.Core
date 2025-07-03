74f4b9cb1798ed6f3bac05f5b19fbd0b5e0d579b
Robert J Serrano <wolfieca.rs@gmail.com>
2025-07-03 15:13:14 -0700

    June 2025

--- Wolf1.Core/Legal/LegalAction.cs
+++ Wolf1.Core/Legal/LegalAction.cs
@@ -6,6 +6,7 @@ using Wolf1.Core.Management;
 using Wolf1.Core.Model;
 using Wolf1.Core.System;
 
+
 namespace Wolf1.Core.Legal
 {
     public enum CourtClass
@@ -15,7 +16,7 @@ namespace Wolf1.Core.Legal
         District = 0b0010,
         Region = 0b0011
     }
-    
+
     public record Court
     {
         public string Name { get; }
@@ -26,10 +27,24 @@ namespace Wolf1.Core.Legal
         public string CourtExtra { get; }
         public string Comment { get; }
     }
+
+    /// <summary>
+    /// LegalStatuses that can apply to a suit
+    ///     SUIT    Suit has been filed
+    ///     DISM    Suit has been dismissed
+    ///     JUDG    Judgment has been entered
+    ///     EXEC    Judgment is being executed
+    ///     SATI    Judgment has been satisfied
+    ///     BKCL    Bankruptcy
+    ///     PRES    
+    ///     RNEW    Judgment to be renewed
+    /// </summary>
     public enum LegalStatus
     {
         DISM, JUDG, EXEC, SUIT, SATI, BKCL, PRES, RNEW
     }
+
+
     public record LegalAction : ILegalAction
     {
         public LegalAction()
@@ -38,18 +53,29 @@ namespace Wolf1.Core.Legal
             SuitDate = DateTime.Now;
         }
         public LegalStatus LegalStatus { get; private set; }
-        public Account? PrimaryAccount { get; private set; }
+        public Account PrimaryAccount { get; private set; } = null;
         public List<Account> IncludedDebts { get; private set; }
         public DateTime SuitDate { get; private set; }
         public Money SuitAmount { get; private set; }
         public Attorney Attorney { get; private set; }
         public Court Court { get; private set; }
         public Dictionary<string, ILegal> LegalData { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="updateTo"></param>
+        /// <returns></returns>
         public LegalAction UpdateLegalStatus(LegalStatus updateTo)
         {
             this.LegalStatus = updateTo;
             return this;
         }
+
+        /// <summary>
+        /// Add a single account to the LegalAction
+        /// </summary>
+        /// <param name="accountToAdd"></param>
+        /// <returns></returns>
         public LegalAction AddAccount(Account accountToAdd)
         {
             if (PrimaryAccount is not null)
@@ -59,11 +85,22 @@ namespace Wolf1.Core.Legal
 
             return this;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="accountListToAdd"></param>
+        /// <returns></returns>
         public LegalAction AddAccount(List<Account> accountListToAdd)
         {
             IncludedDebts.AddRange(accountListToAdd);
             return this;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newPrimaryAccount"></param>
+        /// <returns></returns>
         public LegalAction SetPrimaryAccount(Account newPrimaryAccount)
         {
             if (PrimaryAccount is not null)
@@ -82,11 +119,20 @@ namespace Wolf1.Core.Legal
                 return this;
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newDate"></param>
+        /// <returns></returns>
         public LegalAction SetSuitDate(DateTime newDate)
         {
             SuitDate = newDate;
             return this;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public LegalAction CalculateSuitAmount()
         {
             SuitAmount = PrimaryAccount.TotalOwed[MoneyTypes.Principal] + PrimaryAccount.TotalOwed[MoneyTypes.CollectionCharge] +
@@ -94,11 +140,21 @@ namespace Wolf1.Core.Legal
             PrimaryAccount.TotalOwed[MoneyTypes.AttorneyFees] + PrimaryAccount.TotalOwed[MoneyTypes.LegalFees];
             return this;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assignedAttorney"></param>
+        /// <returns></returns>
         public LegalAction AssignToAttorney(Attorney assignedAttorney)
         {
             Attorney = assignedAttorney;
             return this;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assignedCourt"></param>
+        /// <returns></returns>
         public LegalAction SuitAssignedToCourt(Court assignedCourt)
         {
             Court = assignedCourt;
